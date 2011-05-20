9ac3a140f62bf3267c234c34476ed53c9c5ac215
Andrea Zito <zito.andrea@gmail.com>
2011-05-20 17:47:54 +0200

    Added commodity metadata class

---
+++ src/cloudypeer/Metadata.java
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (c) 2011 Andrea Zito
+ *
+ *  This is free software; see lgpl-2.1.txt
+ */
+package cloudypeer;
+
+import java.util.Hashtable;
+
+/**
+ * Commodity class that defines generic metadata
+ */
+public class Metadata extends Hashtable<String, Object> {}
